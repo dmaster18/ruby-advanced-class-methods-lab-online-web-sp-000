0d3b46a90f999355ddd597b5da50e53929df122b
David Strauch <dmaster18@gmail.com>
2019-08-30 16:22:50 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -50,7 +50,7 @@ class Song
      artist_and_song = file_name.split(/[-"]/)
      artist = artist_and_song[0].to_s.strip
      song.artist_name = artist
-     song_name = artist_and_song[1].to_s.chomp(".mp3").
+     song_name = artist_and_song[1].to_s.chomp(".mp3").strip
      song.name = song_name
      song
   end
