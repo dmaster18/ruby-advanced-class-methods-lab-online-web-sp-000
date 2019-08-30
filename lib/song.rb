8959127f7a5890f97aeec15a78e15cb5d479589c
David Strauch <dmaster18@gmail.com>
2019-08-30 16:15:34 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -48,7 +48,7 @@ class Song
   def self.new_from_filename(file_name)
      song = self.new
      artist_and_song = file_name.split(/[-"]/)
-     artist = artist_and_song[0].to_s
+     artist = artist_and_song[0].to_s.strip
      song.artist_name = artist
      song_name = artist_and_song[1].to_s.strip
      if song_name.include?(".mp3")
